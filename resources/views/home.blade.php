2e74a560409215a181ccd7c239d91286a27b076b
Nikolas Guedes da Silva <guedesnikolas@gmail.com>
2025-06-03 22:08:02 -0300

    blade: laravel_studies_2

--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main_layout')
-@section('content')
-
-@if(!empty($MyName))
-<p class="display-6 text-secondary text-center py-5">{{$MyName}}</p>
-@endif
-
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,32 +3,6 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [MainController::class, 'index']);
-
-Route::view('/view2', 'home', ['MyName' => "Nikolas Guedes"]);
-
-//Rota com parametro
-Route::get('/valor/{value}', [MainController::class, 'mostrarValor']);
-
-//Rota com nome
-Route::get('/rota_abc', function(){
-    return 'Rota Nomeada';
-})->name('rota_nomeada');
-
-Route::get('/rota_referenciada', function(){
-    return redirect()->route('rota_nomeada');
-});
-
-//Rota com Prefix
-Route::prefix('admin')->group(function(){
-    Route::get('/home', [MainController::class, 'index']);
-    Route::get('/about', [MainController::class, 'about']);
-    Route::get('/management', [MainController::class, 'management']);
-});
-
-//Rota com Prefix
-Route::prefix('admin')->group(function(){
-    Route::get('/home', [MainController::class, 'index']);
-    Route::get('/about', [MainController::class, 'about']);
-    Route::get('/management', [MainController::class, 'management']);
+Route::get("/", function () {
+    echo "Ola mundo";
 });
